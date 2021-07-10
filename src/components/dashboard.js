fc02215a9486dcc65a52959decb27a8ec3f9eefc
Dustin Wurtz <dtwurtz@gmail.com>
2021-07-10 15:20:13 -0700

    Designed question component

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,22 +1,20 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import Question from "./question";
+import { Row, Col } from "react-bootstrap";
 
 class Dashboard extends Component {
     render() {
         console.log(this.props)
         return (
-            <div>
-                <h1>All Questions</h1>
-                {this.props.questionIds.map((id) => (
-                    <Question key={id} id={id}/>
-                ))}
-
-                <h1>Unanswered Questions</h1>
-
-                <h1>Answered Questions</h1>
-
-            </div>
+            <Row>
+                <Col>
+                    <h1>All Questions</h1>
+                    {this.props.questionIds.map((id) => (
+                        <Question key={id} id={id}/>
+                    ))}
+                </Col>
+            </Row>
         )
     }
 }
--- src/components/question.js
+++ src/components/question.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { formatQuestion } from "../utils/helpers";
+import { Row, Col } from "react-bootstrap";
 
 class Question extends Component {
     render() {
@@ -11,21 +12,47 @@ class Question extends Component {
         } = question;
 
         return (
-            <div className="question">
-                <div className="question-name">
-                    {name} asks:
-                </div>
-                <div>
-                    <div>
-                        <img src={avatar} />
-                    </div>
-                    <div>
-                        <h4>Would you rather</h4>
-                        <p>...{optionOne.text}...</p>
-                        <button>View Poll</button>
-                    </div>
-                </div>
-            </div>
+            <Row className="question">
+                <Col>
+                    <Row className="question-header">
+                        <Col>
+                            <h5 className="question-name">{name} asks:</h5>
+                        </Col>
+                    </Row>
+                    <Row className="question-body">
+                        <Col md={5} className="question-divder">
+                            <Row>
+                                <Col></Col>
+                                <Col>
+                                    <img src={avatar} className="avatar" />
+                                </Col>
+                                <Col></Col>
+                            </Row>
+                        </Col>
+                        <Col md={7}>
+                            <Row>
+                                <Col>
+                                    <Row className="margin-bottom">
+                                        <Col>
+                                            <h4 className="question-title">Would you rather</h4>
+                                        </Col>
+                                    </Row>
+                                    <Row className="margin-bottom">
+                                        <Col>
+                                            <p className="question-text">...{optionOne.text}...</p>
+                                        </Col>
+                                    </Row>
+                                    <Row>
+                                        <Col>
+                                            <button className="question-botton">View Poll</button>
+                                        </Col>
+                                    </Row>
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                </Col>
+            </Row>
         )
     }
 }
