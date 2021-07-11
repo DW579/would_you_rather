2798b83b0a795425fd7aa36272781eb8f159e1f5
Dustin Wurtz <dtwurtz@gmail.com>
2021-07-10 18:26:03 -0700

    question list sorting questions

--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import Question from "./question";
 import QuestionList from "./question_list";
 import { Row, Col } from "react-bootstrap";
 
@@ -8,27 +7,22 @@ import { Row, Col } from "react-bootstrap";
 
 class Dashboard extends Component {
     render() {
-        console.log(this.props)
         return (
             <Row>
                 <Col>
-                    <h1>All Questions</h1>
-                    {/* {this.props.questionIds.map((id) => (
-                        <Question key={id} id={id}/>
-                    ))} */}
-                    <QuestionList />
+                    <QuestionList/>
                 </Col>
             </Row>
         )
     }
 }
 
-// function mapStateToProps ({ questions }) {
-//     return {
-//         questionIds: Object.keys(questions)
-//             .sort((a,b) => questions[b].timestamp - questions[a].timestamp)
-//     }
-// }
+// Find a way to remove or pull in other data
+function mapStateToProps ({ questions }) {
+    return {
+        questionIds: Object.keys(questions)
+            .sort((a,b) => questions[b].timestamp - questions[a].timestamp)
+    }
+}
 
-
-export default Dashboard;
+export default connect(mapStateToProps)(Dashboard)
--- src/components/question_list.js
+++ src/components/question_list.js
@@ -3,24 +3,78 @@ import { connect } from "react-redux";
 import Question from "./question";
 import { Row, Col } from "react-bootstrap";
 
-class QuestionList extends Component () {
+class QuestionList extends Component {
+    state = {
+        unanswered_selected: true
+    }
+
+    unansweredSelectedTab() {
+        this.setState({
+            unanswered_selected: true
+        })
+    }
+
+    answeredSelectedTab() {
+        this.setState({
+            unanswered_selected: false
+        })
+    }
+
     render() {
+        let unanswered_ids = [];
+        let answered_ids = [];
+        const idVotes = this.props.idVotes;
+        const authedUser = this.props.authedUser;
+
+        for(let i = 0; i < idVotes.length; i++) {
+            if(idVotes[i][1].find(user => user === authedUser) === undefined) {
+                unanswered_ids.push(idVotes[i][0]);
+            }
+            else {
+                answered_ids.push(idVotes[i][0]);
+            }
+        }
+
         return (
             <Row>
                 <Col>
-                    {this.props.questionIds.map((id) => (
-                        <Question key={id} id={id}/>
-                    ))}
+                    <Row>
+                        <Col onClick={() => this.unansweredSelectedTab()} className={this.state.unanswered_selected ? "question-unanswered question-list-tab tab-selected" : "question-unanswered question-list-tab"}>
+                            <h5 className="tab-title">Unanswered Questions</h5>
+                        </Col>
+                        <Col onClick={() => this.answeredSelectedTab()} className={this.state.unanswered_selected ? "question-answered question-list-tab" : "question-answered question-list-tab tab-selected"}>
+                            <h5 className="tab-title">Answered Questions</h5>
+                        </Col>
+                    </Row>
+                    <Row className="question-list-body">
+                        <Col>
+                            {this.state.unanswered_selected 
+                                ? unanswered_ids.map((id) => (
+                                    <Question key={id} id={id}/>
+                                ))
+                                : answered_ids.map((id) => (
+                                    <Question key={id} id={id}/>
+                                ))}
+                        </Col>
+                    </Row>
                 </Col>
             </Row>
         )
     }
 }
 
-function mapStateToProps ({ questions }) {
+function mapStateToProps ({ questions, authedUser }) {
+    let idVotes = [];
+
+    for (const [key, value] of Object.entries(questions)) {
+        const votes = value.optionOne.votes.concat(value.optionTwo.votes);
+
+        idVotes.push([key, votes]);
+    }
+
     return {
-        questionIds: Object.keys(questions)
-            .sort((a,b) => questions[b].timestamp - questions[a].timestamp)
+        idVotes,
+        authedUser
     }
 }
 
